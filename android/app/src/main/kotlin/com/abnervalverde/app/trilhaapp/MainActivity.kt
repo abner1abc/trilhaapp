fbd1a03d967e92eebdc84f081b1157afe8379246
Abner Valverde Cunha <abnercunha2247@gmail.com>
2025-05-12 17:13:53 -0300

    commit incial

---
+++ android/app/src/main/kotlin/com/abnervalverde/app/trilhaapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abnervalverde.app.trilhaapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:trilhaapp/service/gerador_numero_aleatorio_service.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var numeroGerado = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Meu app",
+          style: GoogleFonts.roboto(),
+        ),
+      ),
+      body: Center(
+        child: Text(
+          numeroGerado.toString(),
+      style: GoogleFonts.acme(fontSize: 20),
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add_box),
+        onPressed: () {
+          setState(() {
+            numeroGerado =
+                GeradorNumeroAleatorioService.gerarNumeroAleatorio(1000);
+          });
+        },
+      ),
+    );
+  }
+}
