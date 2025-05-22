1c0738d50c5a24eb3573feb41b0809d3cc7a4e5b
Abner Valverde Cunha <abnercunha2247@gmail.com>
2025-05-21 21:39:47 -0300

    modfied one turn code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -30,43 +30,66 @@ class _HomePageState extends State<HomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(
-              "Ações do usuário",
-              style: GoogleFonts.acme(fontSize: 20),
+            Container(
+              color: Colors.cyan,
+              child: Text(
+                "Ações do usuário",
+                style: GoogleFonts.acme(fontSize: 20),
+              ),
             ),
-            Text(
-              "Foi cliado $quantidadedeCliques vezes",
-              style: GoogleFonts.acme(fontSize: 20),
+            Expanded(
+              child: Container(
+                color: Colors.orange,
+                child: Text(
+                  "Foi cliado $quantidadedeCliques vezes",
+                  style: GoogleFonts.acme(fontSize: 20),
+                ),
+              ),
             ),
-            Text(
-              "O número gerado foi $numeroGerado",
-              style: GoogleFonts.acme(fontSize: 20),
+            Container(
+              color: Colors.indigo,
+              child: Text(
+                "O número gerado foi $numeroGerado",
+                style: GoogleFonts.acme(fontSize: 20),
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  color: Colors.red,
-                  child: Text(
-                    "10",
-                    style: GoogleFonts.acme(fontSize: 20),
+            Container(
+              color: Colors.grey,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.red,
+                      child: Text(
+                        "Nome:",
+                        style: GoogleFonts.acme(fontSize: 20),
+                      ),
+                    ),
                   ),
-                ),
-                Container(
-                  color: Colors.blue,
-                  child: Text(
-                    "20",
-                    style: GoogleFonts.acme(fontSize: 20),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Colors.blue,
+                      child: Text(
+                        "Abner Valverde",
+                        style: GoogleFonts.acme(fontSize: 20),
+                      ),
+                    ),
                   ),
-                ),
-                Container(
-                  color: Colors.green,
-                  child: Text(
-                    "30",
-                    style: GoogleFonts.acme(fontSize: 20),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.green,
+                      child: Text(
+                        "30",
+                        style: GoogleFonts.acme(fontSize: 20),
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             )
           ],
         ),
