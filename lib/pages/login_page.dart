e723230766bed423ba479ea433935585cbbf83d8
Abner Valverde Cunha <abnercunha2247@gmail.com>
2025-07-22 21:22:39 -0300

    update login page and add any img

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,6 +10,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String email = "";
   String senha = "";
+  bool isObscureText = true;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -97,31 +98,41 @@ class _LoginPageState extends State<LoginPage> {
                     height: 30,
                     alignment: Alignment.center,
                     child: TextField(
+                      obscureText: isObscureText,
                       onChanged: (value) {
                         senha = value;
                       },
                       style: const TextStyle(color: Colors.white),
-                      decoration: const InputDecoration(
-                          contentPadding: EdgeInsets.only(top: -3),
-                          enabledBorder: UnderlineInputBorder(
+                      decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.only(top: -3),
+                          enabledBorder: const UnderlineInputBorder(
                             borderSide: BorderSide(
                               color: Color.fromARGB(255, 111, 9, 194),
                             ),
                           ),
-                          focusedBorder: UnderlineInputBorder(
+                          focusedBorder: const UnderlineInputBorder(
                             borderSide: BorderSide(
                               color: Color.fromARGB(255, 111, 9, 194),
                             ),
                           ),
                           hintText: "Senha",
-                          hintStyle: TextStyle(color: Colors.white),
-                          prefixIcon: Icon(
+                          hintStyle: const TextStyle(color: Colors.white),
+                          prefixIcon: const Icon(
                             Icons.lock,
                             color: Color.fromARGB(255, 111, 9, 194),
                           ),
-                          suffixIcon: Icon(
-                            Icons.visibility_off,
-                            color: Color.fromARGB(255, 237, 236, 238),
+                          suffixIcon: InkWell(
+                            onTap: () {
+                              setState(() {
+                                isObscureText = !isObscureText;
+                              });
+                            },
+                            child: Icon(
+                              isObscureText
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                              color: const Color.fromARGB(255, 237, 236, 238),
+                            ),
                           )),
                     )),
                 const SizedBox(
