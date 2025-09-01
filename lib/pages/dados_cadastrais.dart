705ca365ee530359e38d6e45145d820e7d654350
Abner Valverde Cunha <abnercunha2247@gmail.com>
2025-09-01 18:28:30 -0300

    update dados cadastrais and add news edit text

--- lib/pages/dados_cadastrais.dart
+++ lib/pages/dados_cadastrais.dart
@@ -1,26 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/shared/wigets/text_label.dart';
 
-class DadosCadastraisPage extends StatelessWidget {
-  final String texto;
-  final List<String> dados;
-  const DadosCadastraisPage(
-      {super.key, required this.texto, required this.dados});
+class DadosCadastraisPage extends StatefulWidget {
+  const DadosCadastraisPage({
+    super.key,
+  });
+
+  @override
+  State<DadosCadastraisPage> createState() => _DadosCadastraisPageState();
+}
+
+class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
+  var nomeController = TextEditingController(text: "");
+  var dataNascimentoController = TextEditingController(text: "");
+  DateTime? dataNascimento;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(texto),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+      appBar: AppBar(title: const Text("Meus dados")),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "Nome",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+            const TextLabel(texto: "Nome"),
+            TextField(
+              controller: nomeController,
             ),
-            TextField()
+            const SizedBox(
+              height: 15,
+            ),
+            const TextLabel(texto: "Data de Nascimento"),
+            TextField(
+              controller: dataNascimentoController,
+              readOnly: true,
+              onTap: () async {
+                var data = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime(2000, 1, 1),
+                    firstDate: DateTime(1900, 5, 20),
+                    lastDate: DateTime(2023, 10, 23));
+                if (data != null) {
+                  dataNascimentoController.text = data.toString();
+                  dataNascimento = data;
+                }
+                ;
+              },
+            ),
+            TextButton(
+                onPressed: () {
+                  print(nomeController.text);
+                  print(dataNascimento);
+                },
+                child: const Text("salvar")),
           ],
         ),
       ),
